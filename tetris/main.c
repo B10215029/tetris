2ef8a6f37e084b031804b88d427bc12db7e62c0d
B10215029 <B10215029@mail.ntust.edu.tw>
2017-08-25 13:03:27 +0800

    render cursor

--- tetris/main.c
+++ tetris/main.c
@@ -1,52 +1,53 @@
 #pragma warning(disable : 4996)
+#define FPS 60
+#define RANDER_WIDTH 100
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 #include <time.h>
+#include <string.h>
+char* renderBuffer;
+
+	//system("cls");
+	for (int i = 0; i < RENDER_HEIGHT; i++) {
+		renderBuffer[RANDER_WIDTH * i + RANDER_WIDTH - 1] = '\n';
+	}
+	renderBuffer[x + RANDER_WIDTH * y] = '#';
+	puts(renderBuffer);
+}
 
 int main(int argc, char* argv[]) {
-	printf("hello\n");
-	char pr;
-	time_t timer;
-	struct tm y2k = { 0 };
-	double seconds;
-
-	y2k.tm_hour = 0;   y2k.tm_min = 0; y2k.tm_sec = 0;
-	y2k.tm_year = 100; y2k.tm_mon = 0; y2k.tm_mday = 1;
-
-	time(&timer);  /* get current time; same as: timer = time(NULL)  */
-	seconds = difftime(timer, mktime(&y2k));
-
-	double t = ((double)clock()) / CLOCKS_PER_SEC;
+	renderBuffer = malloc(sizeof(char) * RANDER_WIDTH * RENDER_HEIGHT);
+	double lastUpdateTime = ((double)clock()) / CLOCKS_PER_SEC;
+	double accumulationTime = ((double)clock()) / CLOCKS_PER_SEC;
 	while (1) {
+		double currentTime = ((double)clock()) / CLOCKS_PER_SEC;
+		double biasTime = (1.0 / FPS) - (currentTime - accumulationTime);
 		while (kbhit()) {
-			pr = getch();
-			cprintf("\"%d\"", pr);
+			//cprintf("\"%d\"", key);
+			char key = getch();
+			if (key == 'w') {
+				y--;
+			}
+			if (key == 'a') {
+				x--;
+			}
+			if (key == 's') {
+				y++;
+			}
+			if (key == 'd') {
+				x++;
+			}
 		}
-		double nt = ((double)clock()) / CLOCKS_PER_SEC;
-		double dt = nt - t;
-		double wt = (1.0 / 60.0) + ((1.0 / 60.0) - dt);
-		//printf("%d\n", dt);
-		t = nt + (1.0 / 60.0) - dt;
-		
-		//if ((1.0 / 60.0) - dt <= 0) {
-		//	t += (1.0 / 60.0) - dt;
-		//}
-		time(&timer);  /* get current time; same as: timer = time(NULL)  */
-		double newseconds = difftime(timer, mktime(&y2k));
-		if ((int)newseconds != (int)seconds) {
-			putch('|');
-		}
-		else {
-			putch((1.0 / 60.0) - dt <= 0 ? '*' : '.');
-			//putch('.');
-		}
-		seconds = newseconds;
-		wt = wt > 0 ? wt : 0;
-		_sleep(wt * 1000);
-		//printf(".");
-		//putch('.');
+		//putch(((int)lastUpdateTime != (int)currentTime) ? '|' : (biasTime <= 0 ? '*' : '.'));
+		//if ((int)lastUpdateTime != (int)currentTime)
+		lastUpdateTime = currentTime;
+		accumulationTime = currentTime + biasTime;
+		double sleepTime = (1.0 / FPS) + biasTime;
+		sleepTime = sleepTime > 0 ? sleepTime : 0;
+		_sleep(sleepTime * 1000);
 	}
+	free(renderBuffer);
 	system("pause");
 	return 0;
 }
