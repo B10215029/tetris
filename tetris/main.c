5d8e9a7599b706da7e9d9333fcbe09ee0563eb75
B10215029 <B10215029@mail.ntust.edu.tw>
2017-08-25 20:47:59 +0800

    add tetris

--- tetris/main.c
+++ tetris/main.c
@@ -1,47 +1,384 @@
 #pragma warning(disable : 4996)
-#define FPS 60
-#define RANDER_WIDTH 100
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 #include <time.h>
 #include <string.h>
-char* renderBuffer;
 
+#define FPS 30
+#define RANDER_WIDTH 100
+#define RENDER_HEIGHT 30
+#define BOARD_WIDTH 10
+#define BOARD_HEIGHT 20
+#define DROP_TIME 1
+
+typedef struct {
+	int x;
+	int y;
+} ScreenPoint;
+
+typedef struct {
+	int x;
+	int y;
+} Point;
+
+typedef struct {
+	int type;
+	Point position;
+	int rotation;
+	Point data[4];
+} BlockInfo;
+
+const ScreenPoint boardPosition = { 12, 3 };
+const ScreenPoint holdPosition = { 2, 3 };
+const ScreenPoint nextPosition = { 34, 3 };
+const ScreenPoint scorePosition = { 2, 20 };
+const Point blockInitPosition = { 4, 0 };
+
+char* renderBuffer;
+char board[BOARD_HEIGHT][BOARD_WIDTH];
+int blockQueue[7 * 2];
+int blockQueueIndex;
+BlockInfo block;
+int holdBlock;
+int score = 0;
+
+void BlockData(BlockInfo* block) {
+	// [][][][]
+	if (block->type == 0) {
+		int data[8] = { -1, 0, 0, 0, 1, 0, 2, 0 };
+		memcpy(block->data, data, sizeof(data));
+	}
+	//   [][]
+	//   [][]
+	else if (block->type == 1) {
+		int data[8] = { 0, 0, 1, 0, 0, -1, 1, -1 };
+		memcpy(block->data, data, sizeof(data));
+	}
+	//   []
+	// [][][]
+	else if (block->type == 2) {
+		int data[8] = { -1, 0, 0, 0, 1, 0, 0, -1 };
+		memcpy(block->data, data, sizeof(data));
+	}
+	// [][]
+	//   [][]
+	else if (block->type == 3) {
+		int data[8] = { 0, 0, 1, 0, -1, -1, 0, -1 };
+		memcpy(block->data, data, sizeof(data));
+	}
+	// []
+	// [][][]
+	else if (block->type == 4) {
+		int data[8] = { -1, 0, 0, 0, 1, 0, -1, -1 };
+		memcpy(block->data, data, sizeof(data));
+	}
+	//   [][]
+	// [][]
+	else if (block->type == 5) {
+		int data[8] = { -1, 0, 0, 0, 0, -1, 1, -1 };
+		memcpy(block->data, data, sizeof(data));
+	}
+	//     []
+	// [][][]
+	else if (block->type == 6) {
+		int data[8] = { -1, 0, 0, 0, 1, 0, 1, -1 };
+		memcpy(block->data, data, sizeof(data));
+	}
+	for (int i = 0; i < 4; i++) {
+		block->data[i].x += block->position.x;
+		block->data[i].y += block->position.y;
+	}
+}
+
+inline int ScreenPointToPosition(ScreenPoint point) {
+	return point.x + point.y * RANDER_WIDTH;
+}
+
+void render() {
 	//system("cls");
+	// clear renderBuffer
+	memset(renderBuffer, 32, RANDER_WIDTH * RENDER_HEIGHT);
 	for (int i = 0; i < RENDER_HEIGHT; i++) {
 		renderBuffer[RANDER_WIDTH * i + RANDER_WIDTH - 1] = '\n';
 	}
-	renderBuffer[x + RANDER_WIDTH * y] = '#';
+	// render board
+	for (int x = 0; x < BOARD_WIDTH; x++) {
+		for (int y = 0; y < BOARD_HEIGHT; y++) {
+			ScreenPoint p = { x * 2, y };
+			if (board[y][x] == 0) {
+				renderBuffer[ScreenPointToPosition(boardPosition) + ScreenPointToPosition(p)] = -95;
+				renderBuffer[ScreenPointToPosition(boardPosition) + ScreenPointToPosition(p) + 1] = -68;
+			}
+			else {
+				renderBuffer[ScreenPointToPosition(boardPosition) + ScreenPointToPosition(p)] = -95;
+				renderBuffer[ScreenPointToPosition(boardPosition) + ScreenPointToPosition(p) + 1] = -67;
+			}
+		}
+	}
+	// render hold
+	renderBuffer[ScreenPointToPosition(holdPosition)+0] = 'H';
+	renderBuffer[ScreenPointToPosition(holdPosition)+2] = 'O';
+	renderBuffer[ScreenPointToPosition(holdPosition)+4] = 'L';
+	renderBuffer[ScreenPointToPosition(holdPosition)+6] = 'D';
+	if (holdBlock != -1) {
+		int iconPosition = ScreenPointToPosition((ScreenPoint) { 2, 3 });
+		BlockInfo block = {
+			.type = holdBlock,
+			.position = (Point) { 0, 0 },
+			.rotation = 0,
+		};
+		BlockData(&block);
+		for (int i = 0; i < 4; i++) {
+			ScreenPoint p = { block.data[i].x * 2, block.data[i].y };
+			renderBuffer[ScreenPointToPosition(holdPosition) + iconPosition + ScreenPointToPosition(p)] = -95;
+			renderBuffer[ScreenPointToPosition(holdPosition) + iconPosition + ScreenPointToPosition(p) + 1] = -67;
+		}
+	}
+	// render next
+	renderBuffer[ScreenPointToPosition(nextPosition) + 0] = 'N';
+	renderBuffer[ScreenPointToPosition(nextPosition) + 2] = 'E';
+	renderBuffer[ScreenPointToPosition(nextPosition) + 4] = 'X';
+	renderBuffer[ScreenPointToPosition(nextPosition) + 6] = 'T';
+	int iconPosition = ScreenPointToPosition((ScreenPoint) { 2, 3 });
+	for (int blockI = 0; blockI < 5; blockI++) {
+		BlockInfo nextBlock = {
+			.type = blockQueue[blockQueueIndex + blockI],
+			.position = (Point) { 0, 0 },
+			.rotation = 0,
+		};
+		BlockData(&nextBlock);
+		for (int i = 0; i < 4; i++) {
+			ScreenPoint p = { nextBlock.data[i].x * 2, nextBlock.data[i].y };
+			renderBuffer[ScreenPointToPosition(nextPosition) + iconPosition + ScreenPointToPosition(p)] = -95;
+			renderBuffer[ScreenPointToPosition(nextPosition) + iconPosition + ScreenPointToPosition(p) + 1] = -67;
+		}
+		iconPosition += RANDER_WIDTH * 3;
+	}
+	// render score
+	renderBuffer[ScreenPointToPosition(scorePosition) + 0] = 'L';
+	renderBuffer[ScreenPointToPosition(scorePosition) + 2] = 'I';
+	renderBuffer[ScreenPointToPosition(scorePosition) + 4] = 'N';
+	renderBuffer[ScreenPointToPosition(scorePosition) + 6] = 'E';
+	char scoreBuffer[10];
+	itoa(score, scoreBuffer, 10);
+	memcpy(renderBuffer + (ScreenPointToPosition(scorePosition) + RANDER_WIDTH * 2), scoreBuffer, sizeof(char) * strlen(scoreBuffer));
+
+	renderBuffer[RANDER_WIDTH * RENDER_HEIGHT - 1] = 0; // EOF
+	// print to screen
 	puts(renderBuffer);
+}
+
+void UpdateBlockData(BlockInfo* block) {
+	for (int i = 0; i < 4; i++) {
+		if (block->data[i].y >= 0) {
+			board[block->data[i].y][block->data[i].x] = 0;
+		}
+	}
+	BlockData(block);
+	for (int i = 0; i < 4; i++) {
+		if (block->data[i].y >= 0) {
+			board[block->data[i].y][block->data[i].x] = 1;
+		}
+	}
+}
+
+// 0°f®É°w 1¶¶®É°w
+void RotateBlock(BlockInfo* block, int direction) {
+	block->rotation = (block->rotation + direction) % 4;
+}
+
+inline int IsInBound(Point point) {
+	return point.x >= 0 && point.x < BOARD_WIDTH && point.y < BOARD_HEIGHT;
+}
+
+int MoveCheck(BlockInfo* block, Point vector) {
+	for (int i = 0; i < 4; i++) {
+		Point newPosition = { block->data[i].x + vector.x , block->data[i].y + vector.y };
+		if (!IsInBound(newPosition)) {
+			return 0;
+		}
+		else if (newPosition.y < 0 || board[newPosition.y][newPosition.x] == 0) {
+			continue;
+		}
+		else {
+			int overlapSelf = 0;
+			for (int otherData = 0; otherData < 4; otherData++) {
+				if (newPosition.x == block->data[otherData].x && newPosition.y == block->data[otherData].y) {
+					overlapSelf = 1;
+					break;
+				}
+			}
+			if (!overlapSelf) {
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+int DropBlock(BlockInfo* block) {
+	Point direction = { 0, 1 };
+	if (MoveCheck(block, direction)) {
+		block->position.y++;
+		UpdateBlockData(block);
+		return 0;
+	}
+	else {
+		return 1;
+	}
+}
+
+BlockInfo CreateBlock(int type) {
+	BlockInfo newBlock = {
+		.type = type,
+		.position = blockInitPosition,
+		.rotation = 0,
+	};
+	BlockData(&newBlock);
+	UpdateBlockData(&newBlock);
+	return newBlock;
+}
+
+BlockInfo NextBlock() {
+	static const char fillRow[10] = { 1, 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 };
+	if (memcmp(board[0], fillRow, sizeof(char) * BOARD_WIDTH) == 0) {
+		memset(board[0], 0, sizeof(char) * BOARD_WIDTH);
+	}
+	for (int i = 1; i < BOARD_HEIGHT; i++) {
+		if (memcmp(board[i], fillRow, sizeof(char) * BOARD_WIDTH) == 0) {
+			for (int c = i; c > 0; c--) {
+				memcpy(board[c], board[c - 1], sizeof(char) * BOARD_WIDTH);
+			}
+			score++;
+		}
+	}
+	return CreateBlock(PopBlockQueue());
+}
+
+void FallBlock(BlockInfo* block) {
+	while (!DropBlock(block));
+	BlockInfo nextBlock = NextBlock();
+	memcpy(block, &nextBlock, sizeof(BlockInfo));
+}
+
+int PopBlockQueue() {
+	int nextBlock = blockQueue[blockQueueIndex++];
+	if (blockQueueIndex == 7) {
+		memcpy(blockQueue, blockQueue + 7, sizeof(int) * 7);
+		// shuffle blockQueue last 7 elements
+		for (int i = 0; i < 7; i++) {
+			int  j = i + rand() / (RAND_MAX / (7 - i) + 1);
+			int temp = blockQueue[j];
+			blockQueue[j] = blockQueue[i];
+			blockQueue[i] = temp;
+		}
+		blockQueueIndex = 0;
+	}
+	return nextBlock;
+}
+
+BlockInfo HoldBlock(BlockInfo* block) {
+	for (int i = 0; i < 4; i++) {
+		if (block->data[i].y >= 0) {
+			board[block->data[i].y][block->data[i].x] = 0;
+		}
+	}
+	if (holdBlock == -1) {
+		holdBlock = block->type;
+		return NextBlock();
+	}
+	else {
+		BlockInfo newBlock = CreateBlock(holdBlock);
+		holdBlock = block->type;
+		return newBlock;
+	}
+}
+
+void ResetData() {
+	// clear board
+	memset(board, 0, BOARD_WIDTH * BOARD_HEIGHT);
+	// initialize blockQueue
+	for (int i = 0; i < 7; i++) {
+		blockQueue[i] = blockQueue[i + 7] = i;
+	}
+	for (int i = 0; i < 7; i++) {
+		int  j = i + rand() / (RAND_MAX / (7 - i) + 1);
+		int temp = blockQueue[j];
+		blockQueue[j] = blockQueue[i];
+		blockQueue[i] = temp;
+		j = i + rand() / (RAND_MAX / (7 - i) + 1);
+		temp = blockQueue[j + 7];
+		blockQueue[j + 7] = blockQueue[i + 7];
+		blockQueue[i + 7] = temp;
+	}
+	blockQueueIndex = 0;
+	// initialize current block
+	block = NextBlock();
+	holdBlock = -1;
+	score = 0;
 }
 
 int main(int argc, char* argv[]) {
 	renderBuffer = malloc(sizeof(char) * RANDER_WIDTH * RENDER_HEIGHT);
-	double lastUpdateTime = ((double)clock()) / CLOCKS_PER_SEC;
+	ResetData();
+	double lastDropTime = ((double)clock()) / CLOCKS_PER_SEC;
 	double accumulationTime = ((double)clock()) / CLOCKS_PER_SEC;
 	while (1) {
 		double currentTime = ((double)clock()) / CLOCKS_PER_SEC;
 		double biasTime = (1.0 / FPS) - (currentTime - accumulationTime);
 		while (kbhit()) {
-			//cprintf("\"%d\"", key);
 			char key = getch();
-			if (key == 'w') {
-				y--;
-			}
-			if (key == 'a') {
-				x--;
-			}
-			if (key == 's') {
-				y++;
-			}
-			if (key == 'd') {
-				x++;
+			switch (key)
+			{
+			case 'z':
+				RotateBlock(&block, 0);
+				break;
+			case 'x':
+				RotateBlock(&block, 1);
+				break;
+			case 'c':
+				block = HoldBlock(&block);
+				break;
+			case ' ':
+				FallBlock(&block);
+				break;
+			case -32:
+				key = getch();
+				if (key == 72) {
+					RotateBlock(&block, 1);
+				}
+				else if (key == 80) {
+					DropBlock(&block);
+				}
+				else if (key == 75) {
+					Point direction = { -1, 0 };
+					if (MoveCheck(&block, direction)) {
+						block.position.x--;
+						UpdateBlockData(&block);
+					}
+				}
+				else if (key == 77) {
+					Point direction = { 1, 0 };
+					if (MoveCheck(&block, direction)) {
+						block.position.x++;
+						UpdateBlockData(&block);
+					}
+				}
+				break;
+			default:
+				break;
 			}
 		}
-		//putch(((int)lastUpdateTime != (int)currentTime) ? '|' : (biasTime <= 0 ? '*' : '.'));
-		//if ((int)lastUpdateTime != (int)currentTime)
-		lastUpdateTime = currentTime;
+		
+		if (currentTime - lastDropTime > DROP_TIME) {
+			if (DropBlock(&block)) {
+				block = NextBlock();
+				printf("%lf\n", currentTime - lastDropTime);
+			}
+			lastDropTime = currentTime;
+		}
+		render();
 		accumulationTime = currentTime + biasTime;
 		double sleepTime = (1.0 / FPS) + biasTime;
 		sleepTime = sleepTime > 0 ? sleepTime : 0;
